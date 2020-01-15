e334704d41548241a00c813a232d6f87a4b11127
Elmeri Selänne <eljuse@utu.fi>
2020-01-15 15:24:53 +0200

    Toiminnallisuutta

--- src/MainController.java
+++ src/MainController.java
@@ -16,6 +16,8 @@ public class MainController implements Initializable {
 	@FXML
 	private Label lab2;
 	@FXML
+	private Label errorlabel;
+	@FXML
 	private MenuItem kilometer;
 	@FXML
 	private MenuItem meter;
@@ -90,22 +92,286 @@ public class MainController implements Initializable {
 	
 	//**********************************************************
 	
-	/** KESKEN!
 	@FXML
 	public void ConvertLength (ActionEvent event) {
-		double apu1;
-		double apu2;
+		
+		errorlabel.setText("");
 		String labelMetric = lab1.getText();
 		String labelImperial = lab2.getText();
+		
+		if (labelMetric.length() == 0 || labelImperial.length() == 0) {
+			errorlabel.setText("Choose units!");
+			return;
+		}
+		
+		if (txtMetricLength.getLength() == 0 && txtImperialLength.getLength() == 0) {
+			errorlabel.setText("Set Value!");
+			return;
+		}
+		
+		
+		
+		// Imperial to Metric
 		if (txtMetricLength.getLength() == 0) {
-			switch () {
-			case (labelMetric.equals("))
-			}		
+			double valueImp = Double.parseDouble(txtImperialLength.getText());
+			// Mile to Kilometer
+			if (labelMetric.equals(kilometer.getText()) && labelImperial.equals(mile.getText())) {
+				convertMileToKm(valueImp);
+			}
+			// Mile to Meter
+			if (labelMetric.equals(meter.getText()) && labelImperial.equals(mile.getText())) {
+				convertMileToM(valueImp);
+			}
+			
+			// Mile to Centimeter
+			if (labelMetric.equals(centimeter.getText()) && labelImperial.equals(mile.getText())) {
+				convertMileToCm(valueImp);
+			}
+			
+			/////////////////////////
+			
+			//Yard to Km
+			if (labelMetric.equals(kilometer.getText()) && labelImperial.equals(yard.getText())) {
+				convertYardToKm(valueImp);
+			}
+			// Yard to Meter
+			if (labelMetric.equals(meter.getText()) && labelImperial.equals(yard.getText())) {
+				convertYardToM(valueImp);
+			}
+			
+			// Yard to Centimeter
+			if (labelMetric.equals(centimeter.getText()) && labelImperial.equals(yard.getText())) {
+				convertYardToCm(valueImp);
+			}
+			
+			/////////////////////////////
+			
+			// Foot to Km 
+			if (labelMetric.equals(kilometer.getText()) && labelImperial.equals(foot.getText())) {
+				convertFootToKm(valueImp);
+			}
+			// Foot to Meter
+			if (labelMetric.equals(meter.getText()) && labelImperial.equals(foot.getText())) {
+				convertFootToM(valueImp);
+			}
+			
+			// Foot to Centimeter
+			if (labelMetric.equals(centimeter.getText()) && labelImperial.equals(foot.getText())) {
+				convertFootToCm(valueImp);
+			}
+			
+		} if (txtImperialLength.getLength() == 0){
+			
+			double valueMet = Double.parseDouble(txtMetricLength.getText());
+			
+			//Metric to Imperial
+			//Kilometers to Miles
+			if (labelMetric.equals(kilometer.getText()) && labelImperial.equals(mile.getText())) {
+				convertKmToMile(valueMet);
+			}
+			//Kilometers to Yards
+			if (labelMetric.equals(kilometer.getText()) && labelImperial.equals(yard.getText())) {
+				convertKmToYard(valueMet);
+			}
+			//Kilometers to Foot
+			if (labelMetric.equals(kilometer.getText()) && labelImperial.equals(foot.getText())) {
+				convertKmToFoot(valueMet);
+			}
+			
+			///////////////
+			
+			//Meters to Miles
+			if (labelMetric.equals(meter.getText()) && labelImperial.equals(mile.getText())) {
+				convertMeterToMile(valueMet);
+			}
+			//Meters to Yards
+			if (labelMetric.equals(meter.getText()) && labelImperial.equals(yard.getText())) {
+				convertMeterToYard(valueMet);
+			}
+			//Meters to Feet
+			if (labelMetric.equals(meter.getText()) && labelImperial.equals(foot.getText())) {
+				convertMeterToFoot(valueMet);
+			}
+			
+			////////////////
+			
+			//Cm to Miles
+			if (labelMetric.equals(centimeter.getText()) && labelImperial.equals(mile.getText())) {
+				convertCmToMile(valueMet);
+			}
+			//Cm to Yards
+			if (labelMetric.equals(centimeter.getText()) && labelImperial.equals(yard.getText())) {
+				convertCmToYard(valueMet);
+			}
+			//Cm to Feet
+			if (labelMetric.equals(centimeter.getText()) && labelImperial.equals(foot.getText())) {
+				convertCmToFoot(valueMet);
+			}
+			
 		}
 	}
-	**/
 	
+	@FXML
+	public void ClearLengthFields (ActionEvent event) {
+		txtMetricLength.setText("");
+		txtImperialLength.setText("");
+	}
 	
+	//************** CONVERT METHODS **********************
+	
+	// Mile to Metric system
+	public void convertMileToKm(double a) {
+		double result = a * 1.609344;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtMetricLength.setText(String.valueOf(answer));
+	}
+
+	public void convertMileToM(double a) {
+		double result = a * 1.609344 * 1000;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtMetricLength.setText(String.valueOf(answer));
+	}
+
+	public void convertMileToCm(double a) {
+		double result = a * 1.609344 * 1000 * 100;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtMetricLength.setText(String.valueOf(answer));
+	}
+
+	// Yard in Metric system
+	public void convertYardToKm(double a) {
+		double result = a * 0.0009144;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtMetricLength.setText(String.valueOf(answer));
+	}
+
+	public void convertYardToM(double a) {
+		double result = a * 0.9144;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtMetricLength.setText(String.valueOf(answer));
+	}
+
+	public void convertYardToCm(double a) {
+		double result = a * 91.44;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtMetricLength.setText(String.valueOf(answer));
+
+	}
+
+	// Foot to Metric system
+	public void convertFootToKm(double a) {
+		double result = a * 0.0003048;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtMetricLength.setText(String.valueOf(answer));
+	}
+
+	public void convertFootToM(double a) {
+		double result = a * 0.3048;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtMetricLength.setText(String.valueOf(answer));
+	}
+
+	public void convertFootToCm(double a) {
+		double result = a * 30.48;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtMetricLength.setText(String.valueOf(answer));
+
+	}
+	
+	//Km to Imperial system
+	public void convertKmToMile(double a) {
+		double result = a / 1.609344;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtImperialLength.setText(String.valueOf(answer));
+	}
+	
+	public void convertKmToYard(double a) {
+		double result = a * 1093.6133;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtImperialLength.setText(String.valueOf(answer));
+	}
+	
+	public void convertKmToFoot(double a) {
+		double result = a * 3280.8399;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtImperialLength.setText(String.valueOf(answer));
+	}
+	
+	//Metre to Imperial system
+	
+	public void convertMeterToMile(double a) {
+		double result = (a / 1.609344) / 1000;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtImperialLength.setText(String.valueOf(answer));
+	}
+	
+	public void convertMeterToYard(double a) {
+		double result = a * 1.0936133;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtImperialLength.setText(String.valueOf(answer));
+	}
+	
+	public void convertMeterToFoot(double a) {
+		double result = a * 3.2808399;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtImperialLength.setText(String.valueOf(answer));
+	}
+
+	// Cm to Imperial system
+	
+	public void convertCmToMile(double a) {
+		double result = (a / 1.609344) / 1000 / 1000;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtImperialLength.setText(String.valueOf(answer));
+	}
+	
+	public void convertCmToYard(double a) {
+		double result = a * 1.0936133 / 100;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtImperialLength.setText(String.valueOf(answer));
+	}
+	
+	public void convertCmToFoot(double a) {
+		double result = a * 3.2808399 / 100;
+		String answer;
+		result = Math.round(result * 100.0) / 100.0;
+		answer = String.valueOf(result);
+		txtImperialLength.setText(String.valueOf(answer));
+	}
 	
 	
 	
